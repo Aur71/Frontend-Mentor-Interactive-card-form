4667ba827e0ab0bc00d16c2f93301f852a4d9404
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-16 13:29:44 +0300

    Some js

--- app.js
+++ app.js
@@ -9,7 +9,40 @@ const cardMonth = document.getElementById("month");
 const cardYear = document.getElementById("year");
 const cardCVC = document.getElementById("CVC");
 
-button.addEventListener("click", () => {
-    form.style.display = "none";
-    messege.style.display = "flex";
+
+formName.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^a-z\s]/gi, '');
+    e.target.value = e.target.value.replace(/[a-z\s]{21}/i, '');
+    paragraphs[1].textContent = e.target.value
+});
+
+cardNumber.addEventListener("input", (e) => {
+    paragraphs[0].textContent = e.target.value
 })
+
+cardMonth.addEventListener("input", (e) => {
+    if(e.target.value < 1 || e.target.value > 12) {
+        e.target.value = e.target.value.replace('');
+    }
+    paragraphs[2].textContent = e.target.value;
+})
+
+cardYear.addEventListener("input", (e) => {
+    if(e.target.value.length > 2) {
+        e.target.value = e.target.value.replace('');
+    }
+})
+
+cardCVC.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/\d{4}/, '');
+    if(e.target.value.length < 3) {
+        button.addEventListener("click", () => {
+            errors[0].style.display = "block"
+        })
+    }
+})
+
+// button.addEventListener("click", () => {
+//     form.style.display = "none";
+//     messege.style.display = "flex";
+// })
