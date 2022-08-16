973b30c5fe03cfe7dce02847d1774b62e98d29b3
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-16 12:02:43 +0300

    Styling done

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const paragraphs = document.querySelectorAll("p");
+const button = document.querySelector("button");
+const form = document.querySelector("form");
+const messege = document.querySelector(".messege");
+const errors = document.querySelectorAll(".error");
+const formName = document.getElementById("name");
+const cardNumber = document.getElementById("number")
+const cardMonth = document.getElementById("month");
+const cardYear = document.getElementById("year");
+const cardCVC = document.getElementById("CVC");
+
+button.addEventListener("click", () => {
+    form.style.display = "none";
+    messege.style.display = "flex";
+})
