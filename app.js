082abe906b0f785e51b935e5d2bf5fd0b1ac2e2c
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-16 16:58:50 +0300

    Some more js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const paragraphs = document.querySelectorAll("p");
-const button = document.querySelector("button");
+const button = document.getElementById("confirmBtn")
 const form = document.querySelector("form");
 const messege = document.querySelector(".messege");
 const errors = document.querySelectorAll(".error");
@@ -11,38 +11,47 @@ const cardCVC = document.getElementById("CVC");
 
 
 formName.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(/[^a-z\s]/gi, '');
-    e.target.value = e.target.value.replace(/[a-z\s]{21}/i, '');
+    e.target.value = e.target.value.replace(/[^a-z\s]/gi, '').substring(0, 20);
     paragraphs[1].textContent = e.target.value
 });
 
 cardNumber.addEventListener("input", (e) => {
-    paragraphs[0].textContent = e.target.value
+    e.target.value = e.target.value.replace(/[^\d\s/]/g , "").substring(0, 16);
+    const str = `${e.target.value}`;
+    const arr = str.split("");
+    arr.splice(4, 0, " ");
+    arr.splice(9, 0, " ");
+    arr.splice(14, 0, " ");
+    const result = arr.join("");
+    paragraphs[0].textContent = `${result}`
 })
+
+const dateM = [];
 
 cardMonth.addEventListener("input", (e) => {
     if(e.target.value < 1 || e.target.value > 12) {
-        e.target.value = e.target.value.replace('');
+        e.target.value = e.target.value.replace("")
     }
-    paragraphs[2].textContent = e.target.value;
+    e.target.value = e.target.value.replace(/[^\d\s/]/g , "").substring(0, 2);
+    const month = e.target.value;
+    dateM.push(month)
 })
 
-cardYear.addEventListener("input", (e) => {
-    if(e.target.value.length > 2) {
-        e.target.value = e.target.value.replace('');
-    }
-})
+console.log(dateM)
 
-cardCVC.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(/\d{4}/, '');
-    if(e.target.value.length < 3) {
-        button.addEventListener("click", () => {
-            errors[0].style.display = "block"
-        })
-    }
-})
 
-// button.addEventListener("click", () => {
-//     form.style.display = "none";
-//     messege.style.display = "flex";
-// })
+button.addEventListener("click", () => {
+    if(paragraphs[1].textContent == "Jane Appleseed") {
+        errors[0].style.display = "block";
+        formName.style.border = "1px solid red"
+    } 
+    if(paragraphs[0].textContent == `0000 0000 0000 0000`) {
+        errors[1].style.display = "block";
+        cardNumber.style.border = "1px solid red"
+    }
+    
+    // else {
+    //     form.style.display = "none";
+    //     messege.style.display = "flex";
+    // }
+})
